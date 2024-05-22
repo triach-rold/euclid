60e39e485b78dc001611c58a548020e7c4818c68
Triach Rold <156170660+triach-rold@users.noreply.github.com>
2024-05-23 01:55:13 +0530

    Update sterling_factorial.cpp

--- without_specific_documentation/math/sterling_factorial.cpp
+++ without_specific_documentation/math/sterling_factorial.cpp
@@ -6,6 +6,7 @@
 #define YELLOW_TEXT "\033[1;33m"
 #define RESET_TEXT "\033[0m"
 #define CYAN_TEXT "\033[1;36m"
+#define PURPLE_TEXT "\033[1;35m"
 double sterling_factorial(int n) {
     if (n < 0) {
         std::cerr << RED_TEXT "Error: Factorial of a negative number is undefined."  RESET_TEXT<< std::endl;
@@ -29,10 +30,10 @@ unsigned long long factorial(int n) {
 }
 int main() {
     for (int n = -1; n <= 30; ++n) {
-        std::cout << "n = " << n << std::endl;
+        std::cout <<  PURPLE_TEXT "n = " << n << RESET_TEXT << std::endl;
         std::cout <<  YELLOW_TEXT "Sterling's Approximation: " << sterling_factorial(n) << RESET_TEXT << std::endl;
         std::cout <<  GREEN_TEXT "Factorial: " << factorial(n) << RESET_TEXT << std::endl;
-        std::cout <<  CYAN_TEXT ((factorial(n) - sterling_factorial(n)) / static_cast<double>(factorial(n))) * 100 << RESET_TEXT << std:endl;
+        std::cout <<  CYAN_TEXT << "Error Percentage: " <<  ((factorial(n) - sterling_factorial(n)) / static_cast<double>(factorial(n))) * 100 << "%" << RESET_TEXT << std::endl;
         std::cout << std::endl;
     }
     return 0;
